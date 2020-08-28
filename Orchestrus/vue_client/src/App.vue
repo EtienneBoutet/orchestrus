cd62396d49a4c1d9efd506519269f973c8e939b8
Etienne <etienne.boutet@outlook.com>
2020-08-27 23:51:35 -0400

    ui progress

---
+++ Orchestrus/vue_client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <Menu/>
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Menu
+  }
+}
+</script>
---
+++ Orchestrus/vue_client/src/components/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="images-header">
+    <div>
+      <h1 class="title is-1">Orchestrus</h1>
+    </div>
+    <div>
+      <button>
+        <span class="icon">
+          <i class="fas fa-redo-alt"></i>
+        </span>
+      </button>
+    </div>
+  </div>
+  <Workers/>
+</template>
+
+<script>
+import Workers from './Workers.vue'
+
+export default {
+  name: 'Menu',
+  components: {
+    Workers
+  }
+}
+</script>
