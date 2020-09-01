9b5ab582df5e7e845f5393734074f07ae68238f4
Etienne <etienne.boutet@outlook.com>
2020-09-01 18:45:11 -0400

    fix bugs related to add image endpoint

--- Orchestrus/server/python_db_connexion/schemas/image.py
+++ Orchestrus/server/python_db_connexion/schemas/image.py
@@ -3,13 +3,13 @@ from marshmallow import fields, Schema
 
 class ImageSchema(Schema):
   """Schema for getting an image's information"""
-  id = fields.Integer(required=True)
+  id = fields.String(required=True)
   name = fields.String(required=True)
   port = fields.Raw(required=True)
-  worker_ip = fields.Integer(required=True)
+  worker_ip = fields.String(required=True)
 
 class CreateImageSchema(Schema):
   """Schema for adding an image"""
-  id = fields.Integer(required=True)
+  id = fields.String(required=True)
   name = fields.String(required=True)
   port = fields.Raw(required=True)
